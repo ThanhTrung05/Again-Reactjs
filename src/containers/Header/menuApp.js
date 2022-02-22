7c94a3cc1854a87692c34693c8c5d2e6e9006737
Trung <thanhtrung05101996@gmail.com>
2022-02-22 15:21:23 +0700

    Phân quyền Router và tạo router cho doctor

--- src/containers/Header/menuApp.js
+++ src/containers/Header/menuApp.js
@@ -13,8 +13,14 @@ export const adminMenu = [
                 //     { name: 'menu.system.system-administrator.user-manage', link: '/system/user-manage' },
                 //     { name: 'menu.system.system-administrator.user-redux', link: '/system/user-redux' },
                 // ]
-            }, {
-                name: 'menu.admin.manage-admin', link: '/system/user-admin',
+            },
+            // {
+            //     name: 'menu.admin.manage-admin', link: '/system/user-admin',
+            // },
+
+
+            { //Quản lý kế hoạch khám bệnh của bác sĩ
+                name: 'menu.doctor.manage-schedule', link: '/doctor/manage-schedule',
             },
         ]
     },
@@ -39,4 +45,15 @@ export const adminMenu = [
         },
         ]
     },
+];
+
+export const doctorMenu = [
+    {
+        name: 'menu.admin.manage-user',
+        menus: [
+            { //Quản lý kế hoạch khám bệnh của bác sĩ
+                name: 'menu.doctor.manage-schedule', link: '/doctor/manage-schedule',
+            },
+        ]
+    }
 ];
